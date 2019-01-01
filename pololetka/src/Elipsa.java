5ab58772bf06ca437b1553783ceb25f219b1b0a1
sikula1234 <jirka.badatel@gmail.com>
2019-01-01 12:03:31 +0100

    Ctrl + Shift + F

--- pololetka/src/Elipsa.java
+++ pololetka/src/Elipsa.java
@@ -12,13 +12,14 @@ public class Elipsa {
 		double a = Integer.parseInt(args[0]);
 		double b = Integer.parseInt(args[1]);
 
-		// Pro kazdy souradnicovy bod [x,y] (postupne po radcich) zjisti, zda lezi v elipse
+		// Pro kazdy souradnicovy bod [x,y] (postupne po radcich) zjisti,
+		// zda lezi v elipse
 		for (double y = -b; y <= b; y++) {
 			for (double x = -a; x <= a; x++) {
 				// Pokud bod [x,y] lezi v elipse, vytiskni na nem "X"
 				if (leziVElipse(x, y, a, b)) {
 					System.out.print("X");
-				// Pokud bod [x,y] nelezi v elipse, vytiskni na nem mezeru
+					// Pokud bod [x,y] nelezi v elipse, vytiskni na nem mezeru
 				} else {
 					System.out.print(" ");
 				}
