df4310ec6431868c9d648ee52e939421057fcb15
sikula1234 <jirka.badatel@gmail.com>
2019-04-11 17:10:40 +0200

    Add pololetka2

---
+++ pololetka2/src/cz/alisma/alej/text/wrapping/RightAligner.java
@@ -0,0 +1,70 @@
+/*
+ * MIT License
+ * Copyright (c) 2017 Gymnazium Nad Aleji
+ * Copyright (c) 2017 Vojtech Horky
+ * Copyright (c) 2019 Jiri Heller
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package cz.alisma.alej.text.wrapping;
+
+import java.util.List;
+
+/** Left-aligns a line. */
+public class RightAligner implements Aligner {
+	
+	int width;
+
+	public RightAligner(int w) {
+		width = w;
+	}
+	
+    @Override
+    public String format(List<String> words) {
+        StringBuilder result = new StringBuilder();
+        int lineLength = 0;
+        int spacesInFront = 0;
+        
+        for (String w : words) {
+        	lineLength += w.length() + 1;
+        }
+        
+        lineLength--;
+        
+        spacesInFront = width - lineLength;
+        
+        for (int i = 0; i < spacesInFront; i++) {
+        	result.append(" ");
+        }
+        
+        boolean first = true;
+        for (String w : words) {
+            if (!first) {
+                result.append(" ");
+            } else {
+                first = false;
+            }
+            result.append(w);
+        }
+        
+        return result.toString();
+    }
+
+}
---
+++ pololetka2/src/cz/alisma/alej/text/wrapping/WrapAndAlign.java
@@ -0,0 +1,53 @@
+/*
+ * MIT License
+ * Copyright (c) 2017 Gymnazium Nad Aleji
+ * Copyright (c) 2017 Vojtech Horky
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package cz.alisma.alej.text.wrapping;
+
+import java.util.Scanner;
+
+public class WrapAndAlign {
+    private static final int DEF_MAX_WIDTH = 60;
+    public static int maxWidth;
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ParagraphDetector pd = new ParagraphDetector(input);
+        
+        maxWidth = 60;
+        
+        Aligner aligner = new RightAligner(maxWidth);
+        
+
+        while (pd.hasNextParagraph()) {
+            Paragraph para = pd.nextParagraph();
+            LinePrinter line = new LinePrinter(System.out, maxWidth, aligner);
+            while (para.hasNextWord()) {
+                String word = para.nextWord();
+                line.addWord(word);
+            }
+            line.flush();
+            System.out.println();
+        }
+    }
+}
